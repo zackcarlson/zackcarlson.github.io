bea6663f4bc306348cc9564fb7985899dd585695
zackcarlson <zackt.carlson@gmail.com>
2019-03-08 22:04:17 -0800

    Update style

--- app.js
+++
@@ -1,9 +0,0 @@
-$(function() {
-   $('li a').on('click', function(e) {
-    e.preventDefault();
-    var targetSec = $(this).text();
-    $('html, body').animate({
-       scrollTop: $('#' + targetSec).offset().top
-    }, 1000);
-  });
- });
