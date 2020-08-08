69973488a29b8de2bd837ce7353f9e0f69e1c4ea
Zack Carlson <zackcarlson@IAB-MAC-00012.local>
2020-08-07 18:15:50 -0700

    Create portfolio v2 using React

---
+++ src/App.js
@@ -0,0 +1,10 @@
+import React from "react";
+import { Nav } from "./components/Nav";
+
+export const App = () => {
+  return (
+    <div className="App--container">
+      <Nav />
+    </div>
+  );
+};
